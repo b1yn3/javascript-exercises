c348b9f813c4bdd2d44bc2242bed08a78b9d49a2
Bryne <228299496+b1yn3@users.noreply.github.com>
2025-11-30 17:54:29 +0800

    Solved 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * 5/9;
+  celcius = Math.round(celcius * 10) / 10; // Round to the 1st decimal place
+  return celcius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10; // Round to the 1st decimal place
+  return fahrenheit;
 };
 
 // Do not edit below this line
